1aa219f0974c423de5506c6488e7cbc9d2552d08
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-02-17 10:46:44 +0200

    )

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -14,57 +14,57 @@ const token = {
 
 const register = createAsyncThunk(
     'auth/signup',
-    async (user, thunkAPI) => {
+    async (user, {rejectWithValue}) => {
         try {
             const { data } = await axios.post('/users/signup', user);
             token.set(data.token);
             return data;
         } catch (error) {
-            thunkAPI.rejectWithValue(error.message);
+           return rejectWithValue(error.message);
         }
     }
 );
 
 const logIn = createAsyncThunk(
     'auth/login',
-    async (user, thunkAPI) => {
+    async (user, {rejectWithValue}) => {
         try {
             const { data } = await axios.post('/users/login', user);
             token.set(data.token);
             return data;
         } catch (error) {
             alert("Please check your email or password.");
-            thunkAPI.rejectWithValue(error.message);
+           return rejectWithValue(error.message);
         }
     }
 );
 
 const logOut = createAsyncThunk(
     'auth/logout',
-    async (_, thunkAPI) => {
+    async (_, {rejectWithValue}) => {
         try {
             await axios.post('/users/logout');
             token.unset();
         } catch (error) {
             alert("Please check is your email correct");
-            thunkAPI.rejectWithValue(error.message);
+            return rejectWithValue(error.message);
         }
     }
 );
 const current = createAsyncThunk(
     'auth/current',
-    async (_, thunkAPI) => {
+    async (_, {rejectWithValue}) => {
         const state = thunkAPI.getState();
         const persistedToken = state.auth.token;
         if (!persistedToken) {
-            return thunkAPI.rejectWithValue();
+            return rejectWithValue();
         }
         token.set(persistedToken);
         try {
             const { data } = await axios.get('/users/current');
             return data;
         } catch (error) {
-            thunkAPI.rejectWithValue(error.message);
+            return rejectWithValue(error.message);
         }
     }
 );
--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -5,45 +5,45 @@ axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 export const fetchContacts = createAsyncThunk(
     "contacts/fetchContacts",
-    async (_, thunkAPI) => {
+    async (_, {rejectWithValue}) => {
         try {
             const response = await axios('/contacts');
             return response.data;
         } catch (error) {
-            return thunkAPI.rejectWithValue(error.message)
+            return rejectWithValue(error.message)
         }
     }
 )
 export const addContacts = createAsyncThunk(
     "contacts/addContact",
-    async (contact, thunkAPI) => {
+    async (contact, {rejectWithValue}) => {
         try {
             const response = await axios.post('/contacts', contact)
             return response.data;
         } catch (error) {
-            return thunkAPI.rejectWithValue(error.message)
+            return rejectWithValue(error.message)
         }
     }
 )
 export const deleteContact = createAsyncThunk(
     "contacts/deleteContact",
-    async (id, thunkAPI) => {
+    async (id, {rejectWithValue}) => {
         try {
             const response = await axios.delete(`/contacts/${id}`)
             return response.data
         } catch (error) {
-            return thunkAPI.rejectWithValue(error.message)
+            return rejectWithValue(error.message)
         }
     }
 )
 export const currentContact = createAsyncThunk(
     'contacts/currentContact',
-    async(_, thunkAPI)=>{
+    async(_, {rejectWithValue})=>{
         try {
            const {data} = await axios.get('/contacts')
              return data;
         } catch (error) {
-            thunkAPI.rejectWithValue(error.message);
+           return rejectWithValue(error.message);
         }
     }
 );
